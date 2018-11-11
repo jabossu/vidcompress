6e0ab0652438df6aaa9cd3ab36d869f74b3e79fb
Jacques-Alexandre Bossu <jacquesalexandrebossu@gmail.com>
2018-11-11 16:37:49 +0200

    Made the script slighly more verbose

--- vidcompress.sh
+++ vidcompress.sh
@@ -121,7 +121,7 @@ else
 		-c:a aac -b:a 128k \
 		-c:v libx265 -x265-params log-level=error \
 		-preset $p "$o" \
-	&& $autoremove && rm $inputfile
+	&& $autoremove && rm $inputfile && echo " * Removing source file"
 fi
 
 echo "Exiting. Goodbye"
